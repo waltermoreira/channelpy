9c0eb926dd1acf53c67181581a6ba158d1803800
Walter Moreira <walter@waltermoreira.net>
2015-07-19 16:54:34 -0500

    Add test for close

--- tests/test_channelpy.py
+++ tests/test_channelpy.py
@@ -13,7 +13,10 @@ def anon_ch(request):
     ch = channelpy.Channel(uri=BROKER_URI)
 
     def fin():
-        ch.delete()
+        ch2 = channelpy.Channel(name=ch._name, uri=BROKER_URI)
+        ch2.delete()
+
+    request.addfinalizer(fin)
 
     return ch
 
@@ -22,3 +25,16 @@ def test_anon_ch(anon_ch):
     assert anon_ch.uri == BROKER_URI
     assert anon_ch.put(5) is None
     assert anon_ch.get() == 5
+
+
+def test_close(anon_ch):
+    anon_ch.close()
+    with pytest.raises(channelpy.ChannelClosedException):
+        anon_ch.get()
+    with pytest.raises(channelpy.ChannelClosedException):
+        anon_ch.put(1)
+    with pytest.raises(channelpy.ChannelClosedException):
+        anon_ch.close()
+    with pytest.raises(channelpy.ChannelClosedException):
+        anon_ch.close_all()
+
